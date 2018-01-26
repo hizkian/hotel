fafda201e30f42868ee661369df18221abd64651
hizkian <hiznugroho@gmail.com>
2018-01-27 00:09:23 +0700

    invoice lumayan

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -5,39 +5,54 @@
     <title>invoice</title>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <style media="screen">
-      table{
-        border-collapse: collapse;
-      }
-      td,th{
-        border:1px solid;
-      }
+
     </style>
   </head>
   <body>
-    <h1 class="w3-red">Hotel Agung</h1>
+    <h1 class="">Hotel Agung</h1>
     <hr>
     <ul style="list-style:none;margin-left:-40px">
       <li>Jl. Diponegoro No.9, Genteng Kulon,</li>
       <li>Genteng, Kabupaten Banyuwangi, Jawa Timur 68465</li>
       <li>(0333) 845844</li>
     </ul>
-    <table style="width:100%">
-      <thead>
+    <hr>
+    <ul style="list-style:none;margin-left:-40px">
+      <li style="font-weight:bold">Atas Nama:</li>
+      <li>Hizkia Nugroho</li>
+    </ul>
+    <table class="w3-table w3-striped w3-display-container" style="width:100%">
+
         <tr>
-          <th style="width:350px">Nama Kamar</th>
+          <th style="width:250px">Nama Kamar</th>
           <th>Jumlah menginap</th>
           <th>Harga</th>
           <th>Total</th>
         </tr>
-      </thead>
-      <tbody>
+        <tr>
+          <td class="w3-padding">Anggrek 1</td>
+          <td>5</td>
+          <td>@Rp 200.000</td>
+          <td>Rp 1.000.000</td>
+        </tr>
         <tr>
           <td>Anggrek 1</td>
           <td>5</td>
           <td>@Rp 200.000</td>
           <td>Rp 1.000.000</td>
         </tr>
-      </tbody>
+        <tr>
+          <td>Anggrek 1</td>
+          <td>5</td>
+          <td>@Rp 200.000</td>
+          <td>Rp 1.000.000</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td class="w3-right" style="font-weight:bold">Jumlah</td>
+          <td>Rp 3.000.000</td>
+        </tr>
     </table>
   </body>
 </html>
